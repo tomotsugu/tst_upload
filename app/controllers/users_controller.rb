c309a7a2c3cae241a61b85c92293e5a105608bf6
tom <salem55.tk@gmail.com>
2018-02-10 15:30:05 +0000

    “update”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :check_login, only: [:show, :new, :create]
+  before_action :check_login, only: [:show]
 
   def new
     @user = User.new
