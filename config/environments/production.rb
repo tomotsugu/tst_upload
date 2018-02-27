3f928686873725cd2c9f348c12612214db967f31
tomotsugu <34186561+tomotsugu@users.noreply.github.com>
2018-02-27 23:10:02 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,12 +93,6 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
-    user_name: ENV['sgg502gj@kke.com'],
-    password: ENV['solaa3848'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
+ 
   }
 end
